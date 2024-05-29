e5471a638fcc89c2dfd60540439e29913002603b
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-05-29 17:56:12 +0530

    create slice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,22 +2,30 @@ import React, { useEffect } from 'react'
 import MovieList from "../components/MovieList"
 import MovieApi from '../common/apis/MovieApi'
 import { APIkey } from '../common/apis/MovieApiKey'
+import { useDispatch } from "react-redux"
+import { addMovies } from '../features/movies/MovieSlice'
+
 
 function Home() {
 
+  const movietext="Harry";
+  const dispatch=useDispatch()
+
+
   useEffect(()=>{
-    const movietext="Harry";
+   
     const fetchMovies=async()=>{
       try {
       const response= await MovieApi.get(`?apikey=${APIkey}&s=${movietext}&type=movie`)
-        console.log("The Response :" ,response);
+        dispatch(addMovies(response.data));
       } catch (error) {
         console.log("Error : ",error);
       }
     };
 
-    fetchMovies()
-  },[])
+    fetchMovies();
+    
+  },[dispatch])
 
   return (
     <div className=''>
--- src/features/store.jsx
+++ src/features/store.jsx
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit" 
+import movieReducer from "./movies/MovieSlice"
 
 
 export const store= configureStore({
-    reducer:{},
+    reducer:{
+        movies:movieReducer,
+    },
 })
